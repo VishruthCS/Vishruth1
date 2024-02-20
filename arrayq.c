cf20b30abe1c55e405ab24ad7f9c9234b6663a1e
VishruthCS <160639545+VishruthCS@users.noreply.github.com>
2024-02-20 22:28:46 +0530

    Initial commit

---
+++ arrayq.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    int count = 1;
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] == arr[i + 1]) {
+            count++;
+        } else {
+            printf("%d is repeated %d times.\n", arr[i], count);
+            count = 1; 
+        }
+    }
+    printf("%d is repeated %d times.\n", arr[n - 1], count);
+
+    return 0;
+}
